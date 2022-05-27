013a4becba17e1a8b23ecd37939c3dea4fff5dce
liuml <723164417@qq.com>
2022-05-27 11:47:55 +0800

    add test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -16,4 +16,20 @@ func TestGetCurrentFileDirectory(t *testing.T) {
 	if !ok {
 		t.Error("获取路径失败", path)
 	}
+
+	path, ok = GetFileDirectoryToCaller(1)
+	if !ok {
+		t.Error("获取路径失败", path)
+	}
+}
+
+func TestIf(t *testing.T) {
+	a := 1
+	if 3 != If(a != 1, 1, 3) {
+		t.Error("模拟三元操作失败")
+	}
+
+	if 1 != If(a == 1, 1, 3) {
+		t.Error("模拟三元操作失败")
+	}
 }
