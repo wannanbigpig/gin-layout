a182675fa0db212809f915030dd7c3e6b391ea1d
liuml <723164417@qq.com>
2022-11-04 11:27:59 +0800

    update

--- boot/boot.go
+++ boot/boot.go
@@ -2,25 +2,21 @@ package boot
 
 import (
 	"flag"
-	"fmt"
 	"github.com/wannanbigpig/gin-layout/config"
 	"github.com/wannanbigpig/gin-layout/data"
-	"github.com/wannanbigpig/gin-layout/internal/command"
 	"github.com/wannanbigpig/gin-layout/internal/pkg/logger"
-	"github.com/wannanbigpig/gin-layout/internal/routers"
 	"github.com/wannanbigpig/gin-layout/internal/validator"
 	"os"
-	"strings"
 )
 
 var (
 	configPath   string
 	printVersion bool
-	run          string
+	Run          string
 )
 
 func init() {
-	flag.StringVar(&run, "r", "http", "执行命令默认运行http服务")
+	flag.StringVar(&Run, "r", "http", "执行命令默认运行http服务")
 	flag.StringVar(&configPath, "c", "", "请输入配置文件绝对路径")
 	flag.BoolVar(&printVersion, "v", false, "查看版本")
 	flag.Parse()
@@ -42,23 +38,4 @@ func init() {
 
 	// 4、初始化验证器
 	validator.InitValidatorTrans("zh")
-}
-
-func Run() {
-	script := strings.Split(run, ":")
-	switch script[0] {
-	case "http":
-		r := routers.SetRouters()
-		err := r.Run(fmt.Sprintf("%s:%d", config.Config.Server.Host, config.Config.Server.Port))
-		if err != nil {
-			panic(err)
-		}
-	case "command":
-		if len(script) != 2 {
-			panic("命令错误，缺少重要参数")
-		}
-		command.Run(script[1])
-	default:
-		panic("执行脚本错误")
-	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"github.com/wannanbigpig/gin-layout/boot"
+	"github.com/wannanbigpig/gin-layout/config"
+	"github.com/wannanbigpig/gin-layout/internal/command"
+	"github.com/wannanbigpig/gin-layout/internal/routers"
+	"strings"
 )
 
 func main() {
-	boot.Run()
+	run()
+}
+
+func run() {
+	script := strings.Split(boot.Run, ":")
+	switch script[0] {
+	case "http":
+		r := routers.SetRouters()
+		err := r.Run(fmt.Sprintf("%s:%d", config.Config.Server.Host, config.Config.Server.Port))
+		if err != nil {
+			panic(err)
+		}
+	case "command":
+		if len(script) != 2 {
+			panic("命令错误，缺少重要参数")
+		}
+		command.Run(script[1])
+	default:
+		panic("执行脚本错误")
+	}
 }
